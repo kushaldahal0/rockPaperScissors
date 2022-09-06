961e47fe76ec8358ca6fb428e693f10e6220fa61
Kushal Dahal <kushal.dahal.234@gmail.com>
2022-09-06 20:31:23 +0545

    generalize conditions and add counters

--- rock.js
+++ rock.js
@@ -23,21 +23,38 @@ console.log(`computers choice => ${compSelect}`);
 
 let playerSelect = "ROCK";
 console.log(`players choice => ${playerSelect}(example)`)
+let winCount = 0;
+let loseCount = 0;
+let drawCount = 0;
 
+let win = (a,b) => {
+    console.log(`You Win! ${array[a]} beats ${array[b]}`);
+    winCount ++;
+}
+let loose = (a,b) => {
+    console.log(`You Lose! ${array[1]} beats ${array[0]}`);
+    loseCount ++;
+}
 
+let draw = () =>{
+    console.log("Draw! Both had same choice")
+    drawCount ++;
+}
 
 
 
 let play = (compSelect, playerSelect) => {
-    (compSelect === playerSelect) ? console.log("draw")
-    //win situations
-    : (compSelect == array[0] && playerSelect == array[1]) ? console.log(`You Win! ${array[1]} beats ${array[2]}`)
-    : (compSelect == array[1] && playerSelect == array[2]) ? console.log(`You Win! ${array[2]} beats ${array[1]}`)
-    : (compSelect == array[2] && playerSelect == array[0]) ? console.log(`You Win! ${array[0]} beats ${array[2]}`)
-    //lose situations
-    : (compSelect == array[1] && playerSelect == array[0]) ? console.log(`You Lose! ${array[1]} beats ${array[0]}`)
-    : (compSelect == array[2] && playerSelect == array[1]) ? console.log(`You Lose! ${array[2]} beats ${array[1]}`)
-    : (compSelect == array[0] && playerSelect == array[2]) ? console.log(`You Lose! ${array[0]} beats ${array[2]}`)
+    (compSelect === playerSelect) ? draw()
+    //win situations ------>win(ps, cs)
+    : (compSelect == array[0] && playerSelect == array[1]) ? win(1,0)
+    : (compSelect == array[1] && playerSelect == array[2]) ? win(2,1)
+    : (compSelect == array[2] && playerSelect == array[0]) ? win(0,2)
+    //lose situations ------>loose(ps, cs)
+    : (compSelect == array[1] && playerSelect == array[0]) ? loose(0,1)
+    : (compSelect == array[2] && playerSelect == array[1]) ? loose(1,2)
+    : (compSelect == array[0] && playerSelect == array[2]) ? loose(2,0)
     : console.log("dont know what happened here")
 };
+
 play(compSelect, playerSelect);
+console.log(`wins = ${winCount} , loses = ${loseCount} , draw = ${drawCount}`)
